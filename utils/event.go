2629971b4f04e2b3fda2a866a28758788f82e89e
Jared Ledvina <jared@techsmix.net>
2018-08-30 20:18:12 -0400

    More updates
    Signed-off-by: Jared Ledvina <jared@techsmix.net>

--- utils/event.go
+++ utils/event.go
@@ -6,28 +6,16 @@ import (
 )
 
 type Event struct {
-	ID         string      `json:"id"`
 	Timestamp  int         `json:"timestamp"`
-	Action     string      `json:"action"`
-	Occurences int         `json:"occurrences"`
 	Check      EventCheck  `json:"check"`
 	Entity     EventEntity `json:"entity"`
 }
 
 type EventCheck struct {
-	Type        string   `json:"type"`
 	Name        string   `json:"name"`
 	Command     string   `json:"command"`
-	Subscribers []string `json:"subscribers"`
-	Interval    int      `json:"interval"`
-	Handler     string   `json:"handler"`
-	Handlers    []string `json:"handlers"`
-	Issued      int      `json:"issued"`
 	Output      string   `json:"output"`
-	Status      int      `json:"status"`
-	History     []int    `json:"history"`
-	Source      string   `json:"source"`
-	Origin      string   `json:"origin"`
+	State       string   `json:"state"`
 }
 
 type EventEntity struct {
